1eb872a86a1bdfe33e5aab94815f5cddb7ffa365
robotob <robotob@gmail.com>
2015-10-31 19:54:06 +0700

    reorganize component

---
+++ GenericRepository/Code/GenericRepository/BaseDbContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.Validation;
+using System.Collections.Generic;
+
+namespace ToB.Db.GenericRepository
+{
+    public class BaseDbContext : DbContext 
+    {
+        public BaseDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+            //Database.Connection.ConnectionString = nameOrConnectionString;
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
+           .Where(type => !String.IsNullOrEmpty(type.Namespace))
+           .Where(type => type.BaseType != null && type.BaseType.IsGenericType
+                && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
+            foreach (var type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                modelBuilder.Configurations.Add(configurationInstance);
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+
+                var outputLines = new List<string>();
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    outputLines.Add(string.Format("{0}: Entity of type \"{1}\" in state \"{2}\" has the following validation errors:", DateTime.Now, eve.Entry.Entity.GetType().Name, eve.Entry.State));
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        outputLines.Add(string.Format("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
+                    }
+                }
+                throw e;
+            }
+        }
+        
+    }
+}
---
+++ GenericRepository/Code/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ToB.Db.GenericRepository
+{
+    public interface IGenericRepository<Entity>  where Entity : class
+    {
+        void Add(Entity entity);
+
+        void Delete(object id);
+
+        void Delete(Entity id);
+
+        void Delete(Func<Entity, Boolean> where);
+
+        void Update(Entity entity);
+
+        int Count(Expression<Func<Entity, bool>> filter = null);
+
+        IEnumerable<Entity> GetAll();
+        
+        Entity GetById(object id);
+
+        IQueryable<Entity> GetQueryable(Func<Entity, bool> where);
+
+        bool Exists(object primaryKey);
+
+        Entity GetSingle(Func<Entity, bool> predicate);
+
+        Entity GetFirst(Func<Entity, bool> predicate);
+
+        IEnumerable<Entity> Get(
+            int? pageNo = null, 
+            int? pageSize = null,    
+            Expression<Func<Entity,Entity>> selector = null,
+            Expression<Func<Entity, bool>> filter = null,
+            Func<IQueryable<Entity>, IOrderedQueryable<Entity>> orderBy = null,
+            string includeProperties = "");
+    }
+}
